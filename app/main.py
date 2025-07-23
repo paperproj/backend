aa653fc134c2fc42c3bc4712fb6207f482be7e88
Ninaad <101141056+ninkalla@users.noreply.github.com>
2025-07-23 16:25:11 -0400

    added render-hosted backend to origins in CORS setup

--- app/main.py
+++ app/main.py
@@ -6,10 +6,15 @@ from app.scholar import SemanticScholarClient
 app = FastAPI()
 SemanticScholar = SemanticScholarClient()
 
+origins = [
+    "https://paperproj.github.io",
+    "http://localhost:5173",
+]
+
 # Enable CORS for frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
